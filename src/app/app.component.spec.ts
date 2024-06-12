c4fe73a012335dd0b79c3a28a09f660c5e052b6c
Max Yarosh <jmksm96@gmail.com>
2024-06-13 01:25:15 +0300

    implemented first task

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,26 +1,38 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 describe('AppComponent', () => {
-  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        HelloWorldComponent
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent]
-    });
-    const fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create the component', () => {
-    expect(component).toBeTruthy();
+  it('should create the app', () => {
+    expect(app).toBeTruthy();
   });
 
-  it('should implement OnInit', () => {
-    expect(component.ngOnInit).toBeDefined();
+  it('should render HelloWorld component', () => {
+    const helloWorldElement = fixture.debugElement.query(By.css('app-hello-world'));
+    expect(helloWorldElement).toBeTruthy();
   });
 
-  it('ngOnInit should be a function', () => {
-    expect(typeof component.ngOnInit).toBe('function');
+  it('should render "Hello, World!" text in HelloWorld component', () => {
+    const helloWorldElement = fixture.debugElement.query(By.css('app-hello-world h1'));
+    expect(helloWorldElement.nativeElement.textContent).toContain('Hello, World!');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
+
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import 'zone.js';
+
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  // @ts-ignore
+  if (window['ngRef']) {
+    // @ts-ignore
+    window['ngRef'].destroy();
+  }
+  // @ts-ignore
+  window['ngRef'] = ref;
+
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
+
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// 
+// import { AppModule } from './app/app.module';
+
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
