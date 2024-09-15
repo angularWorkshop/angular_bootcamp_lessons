f8335c50198ec9fd4ebb80cb4b969f8ccae13887
Max Yarosh <jmksm96@gmail.com>
2024-09-15 22:05:52 +0300

    Cleanup files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  title = 'angular-definitive-guide_lessons';
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-
-  }
-}
+export class AppComponent {}
