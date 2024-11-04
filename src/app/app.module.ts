389135374311ae6e26823e436750fa8f9782a4f1
Denis Svirko <denis.svirko@bimeister.com>
2024-11-04 23:20:16 +0300

    AW-91: add structural directives exercise solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
-import { UserFormComponent } from './user-form.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  imports: [CommonModule, RouterOutlet, BrowserModule, UserFormComponent],
-  declarations: [AppComponent],
+  imports: [CommonModule, RouterOutlet, BrowserModule],
+  declarations: [AppComponent, ProductListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+interface Product {
+  name: string;
+  description: string;
+  showDescription: boolean;
+}
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent {
+  products: Product[] = [
+    { name: 'Продукт 1', description: 'Описание продукта 1', showDescription: false },
+    { name: 'Продукт 2', description: 'Описание продукта 2', showDescription: false }
+  ];
+
+  toggleDescription(product: Product): void {
+    product.showDescription = !product.showDescription;
+  }
+}
