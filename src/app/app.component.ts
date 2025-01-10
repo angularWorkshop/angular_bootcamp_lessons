85b95b9920f95ebd30530a33ff78c110e4666dd9
Max Yarosh <jmksm96@gmail.com>
2025-01-10 17:10:47 +0300

    added solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <form #contactForm="ngForm" (ngSubmit)="onSubmit(contactForm.value)">
+      <label for="name">Имя:</label>
+      <input id="name" name="name" ngModel required />
+
+      <label for="email">Email:</label>
+      <input id="email" name="email" ngModel required />
+
+      <button type="submit" [disabled]="contactForm.invalid">Отправить</button>
+    </form>
+  `,
 })
-export class AppComponent {}
+export class AppComponent {
+  onSubmit(formValue: { name: string; email: string }): void {
+    console.log(formValue);
+  }
+}
