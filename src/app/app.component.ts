55cc61f358284cf58bf0513d28918f630cad137a
Max Yarosh <jmksm96@gmail.com>
2026-03-18 21:16:06 +0300

    Add signals counter starter exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected readonly title = 'Order Counter';
+  protected readonly count = signal(0);
+
+  // TODO: make this computed value depend on count
+  protected readonly doubled = computed(() => 0);
+
+  // TODO: update the status based on count
+  protected readonly status = computed(() => 'No orders yet');
+
+  protected increase(): void {
+    // TODO: increase count by 1
+  }
+
+  protected decrease(): void {
+    // TODO: decrease count by 1, but never go below zero
+  }
+}
