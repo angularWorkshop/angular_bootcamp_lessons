73877d17c24d34ddcaa7423ee7ac94eb7432a805
Denis Svirko <denis.svirko@bimeister.com>
2026-03-26 01:08:59 +0300

    Add topic 35 exercise 02 SSR-safe component answer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, OnDestroy, PLATFORM_ID, afterNextRender, inject, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnDestroy {
+  private readonly platformId = inject(PLATFORM_ID);
+
+  readonly viewportWidth = signal<number | null>(null);
+  readonly resizeEvents = signal(0);
+  private hasResizeListener = false;
+
+  private readonly onResize = () => {
+    this.viewportWidth.set(window.innerWidth);
+    this.resizeEvents.update((value) => value + 1);
+  };
+
+  private readonly scheduleBrowserInit = afterNextRender(() => {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
+
+    if (!this.hasResizeListener) {
+      this.onResize();
+      window.addEventListener('resize', this.onResize);
+      this.hasResizeListener = true;
+    }
+  });
+
+  ngOnDestroy() {
+    if (!this.hasResizeListener) {
+      return;
+    }
+
+    window.removeEventListener('resize', this.onResize);
+    this.hasResizeListener = false;
+  }
+}
