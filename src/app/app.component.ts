6b5411a6bb9a35ee658ea3034ea2fe81aebcd467
Denis Svirko <denis.svirko@bimeister.com>
2026-04-09 11:46:27 +0300

    feat: add angular section 19 exercise 43.2 starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected runtime = { production: true, debugToolsEnabled: true, apiBaseUrl: '' };
+  protected errorSurface = '';
+  protected smokeStatus = 'unknown';
+
+  protected runHardeningCheck(): void {
+    // TODO: hide debug tools in production, surface broken config, and compute a smoke status.
+  }
+}
