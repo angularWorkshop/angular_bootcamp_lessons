af5613b3e4e0de5e1c1b28f5d993bb281f68451b
Denis Svirko <denis.svirko@bimeister.com>
2026-04-09 11:46:27 +0300

    feat: add angular section 19 exercise 43.1 solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { RuntimeConfigService } from './runtime-config.service';
+import { TelemetryService } from './telemetry.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  protected errorMessage = '';
+
+  public constructor(
+    protected readonly runtime: RuntimeConfigService,
+    protected readonly telemetry: TelemetryService,
+  ) {}
+
+  protected trackPrimaryAction(): void {
+    this.telemetry.capture('experimental_action_clicked', { env: this.runtime.environment });
+  }
+
+  protected trackError(): void {
+    this.errorMessage = 'The feature action failed.';
+    this.telemetry.capture('experimental_action_failed', { reason: 'manual-test' });
+  }
+}
