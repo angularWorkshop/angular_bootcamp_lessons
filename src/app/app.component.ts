2e0b2292f88f7b1e307ac633ec5d970fb0cc1372
Denis Svirko <denis.svirko@bimeister.com>
2026-08-03 00:27:54 +0300

    Fix Angular 21 exercise validation infrastructure
    # Conflicts:
    #	scripts/stackblitz-status.mjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [RouterModule.forRoot(routes)],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {}
