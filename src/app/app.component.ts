7e696216328913314ad90362439c27e6b0695840
Denis Svirko <denis.svirko@bimeister.com>
2026-08-02 23:48:56 +0300

    Fix model two-way exercise build validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { StarRatingComponent } from './star-rating.component';
-import { ToggleComponent } from './toggle.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [StarRatingComponent, ToggleComponent],
 })
 export class AppComponent {
   protected readonly userRating = signal(0);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
+import { StarRatingComponent } from './star-rating.component';
+import { ToggleComponent } from './toggle.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     RouterOutlet,
-    BrowserModule
+    BrowserModule,
+    StarRatingComponent,
+    ToggleComponent
   ],
   declarations: [AppComponent],
   bootstrap:  [AppComponent]
