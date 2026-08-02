7e9b0afb93c16dd05caefd984781f10f224ab16a
Denis Svirko <denis.svirko@bimeister.com>
2026-08-03 00:15:24 +0300

    Fix Angular 21 standalone test wiring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterOutlet],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {}
